38e95a1e33ba9741a4c6aaec1305b69ad6a99744
759005 <759005@ELF8IT221540.ACDM.DS.SAIT.CA>
2020-09-27 20:46:51 -0600

    Part 1 of Lab 3 Age calculator implemented

---
+++ src/java/servlets/AgeCalculatorServlet.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 759005
+ */
+public class AgeCalculatorServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            String age = request.getParameter("age");
+            int newAge = Integer.parseInt(age) + 1;
+
+            request.setAttribute("message", "Your age next birthday wil be " + newAge);
+
+            getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp").forward(request, response);
+        } catch (NumberFormatException e){
+            
+            request.setAttribute("message", "You must give your current age");
+            getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp").forward(request, response);
+        }
+    }
+
+}
---
+++ web/WEB-INF/ageCalculator.jsp
@@ -0,0 +1,23 @@
+<%-- 
+    Document   : ageCalculator
+    Created on : Sep 27, 2020, 7:37:50 PM
+    Author     : 759005
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Age Calculator</h1>
+        <form action="" method="post">
+            Enter your age <input type="text" name="age"/><br/>
+            <input type="submit" value="Age next birthday"/><br/>
+            <p>${message}</p>
+        </form>
+        <a href="/Week3lab_Calculators/arithmeticCalculator">Arithmetic Calculator</a><br/>
+    </body>
+</html>
